671d66d7354d6b3b08309f81ea4b94046247e1a4
tsteelematc <tsteele@madisoncollege.edu>
2022-11-17 10:42:44 -0600

    Showing filter
    It returns an object so we have to list() to see the items

--- main.py
+++ main.py
@@ -5,3 +5,11 @@
 #
 numbers = [1, 2, 3, 4]
 print(numbers)
+
+numbers_less_than_two = filter(
+    lambda x: x > 2
+    , numbers
+)
+
+print(numbers_less_than_two)
+print(list(numbers_less_than_two))
